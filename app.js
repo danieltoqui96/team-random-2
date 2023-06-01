dabe447e7f88f41dc9524718b02e5cc0646402b6
Daniel Hernández <danieltoqui96@gmail.com>
2023-05-31 21:01:47 -0400

    integrating pokeapi data

--- app.js
+++ app.js
@@ -3,17 +3,79 @@ const buttonTeam = document.getElementById("button-team");
 const teamSection = document.getElementById("team-section");
 
 // Funtions
-
 // Función para obtener seis números al azar no repetidos
-const getRandomNumbers = (min, max) => {
+const getRandomNumbers = (min, max, length) => {
   const numbers = new Set();
-
-  while (numbers.size < 6) {
+  while (numbers.size < length) {
     const randomNum = Math.floor(Math.random() * (max - min + 1)) + min;
     numbers.add(randomNum);
   }
-
   return Array.from(numbers);
+};
+
+// Función para obtener habilidad random
+const getAbility = async (arr) => {
+  const index = Math.floor(Math.random() * arr.length);
+  try {
+    const apiUrl = arr[index].ability.url;
+    const response = await fetch(apiUrl);
+    const data = await response.json();
+    return data.names[5].name;
+  } catch (error) {
+    console.log("error en getAbility: ", error);
+    throw new Error("No se pudo obtener habilidad");
+  }
+};
+
+// Función para obtener naturaleza random
+const getNature = async () => {
+  const id = Math.floor(Math.random() * 25) + 1;
+  try {
+    const apiUrl = `https://pokeapi.co/api/v2/nature/${id}/`;
+    const response = await fetch(apiUrl);
+    const data = await response.json();
+    return data.names[5].name;
+  } catch (error) {
+    console.log("error en getNature: ", error);
+    throw new Error("No se pudo obtener naturaleza");
+  }
+};
+
+// Función para obtener movimiento
+const getMove = async (apiUrl) => {
+  try {
+    const response = await fetch(apiUrl);
+    const data = await response.json();
+    console.log(data.names);
+    const move = {
+      accuracy: data.accuracy,
+      damage_class: data.damage_class.name,
+      name: data.names[5] ? data.names[5].name : data.names[2].name,
+      power: data.power,
+      pp: data.pp,
+      type: data.type.name,
+    };
+    return move;
+  } catch (error) {
+    console.log("error en getMove: ", error);
+    throw new Error("No se pudo obtener movimiento");
+  }
+};
+
+// Función para obtener movimientos random
+const getMoves = async (arr) => {
+  const ids = getRandomNumbers(0, arr.length - 1, 4);
+  const moves = [];
+  try {
+    for (const id of ids) {
+      const move = await getMove(arr[id].move.url);
+      moves.push(move);
+    }
+    return moves;
+  } catch (error) {
+    console.log("error en getMoves: ", error);
+    throw new Error("No se pudo obtener movimientos");
+  }
 };
 
 // Función para obtener un Pokémon dado un número
@@ -22,7 +84,6 @@ const getPokemon = async (number) => {
     const apiUrl = `https://pokeapi.co/api/v2/pokemon/${number}`;
     const response = await fetch(apiUrl);
     const data = await response.json();
-
     const pokemon = {
       id: data.id,
       name: data.name,
@@ -39,7 +100,11 @@ const getPokemon = async (number) => {
         speAtt: data.stats[3].base_stat,
         speDef: data.stats[4].base_stat,
         speed: data.stats[5].base_stat,
+        total: data.stats.reduce((acc, stat) => acc + stat.base_stat, 0),
       },
+      ability: await getAbility(data.abilities),
+      nature: await getNature(),
+      moves: await getMoves(data.moves),
     };
 
     return pokemon;
@@ -51,15 +116,13 @@ const getPokemon = async (number) => {
 
 // Función para obtener seis Pokémon random
 const getRandomTeamData = async () => {
+  const ids = getRandomNumbers(1, 386, 6);
+  const team = [];
   try {
-    const ids = getRandomNumbers(1, 386);
-    const team = [];
-
     for (const id of ids) {
       const pokemon = await getPokemon(id);
       team.push(pokemon);
     }
-
     return team;
   } catch (error) {
     console.log("error en getRandomTeamData: ", error);
@@ -103,6 +166,28 @@ const pokemonHeader = (id, name, types) => {
   return header;
 };
 
+// Función que retorna el color de la barra de stat
+const colorChart = (value) => {
+  if (value < 1 / 6) {
+    return "color1";
+  }
+  if (value < 2 / 6) {
+    return "color2";
+  }
+  if (value < 3 / 6) {
+    return "color3";
+  }
+  if (value < 4 / 6) {
+    return "color4";
+  }
+  if (value < 5 / 6) {
+    return "color5";
+  }
+  if (value <= 6 / 6) {
+    return "color6";
+  }
+};
+
 // Función que retorna el Stat del pokemon
 const pokemonStat = (stat, value, total) => {
   const label = document.createElement("span");
@@ -114,6 +199,10 @@ const pokemonStat = (stat, value, total) => {
   barValue.style.width = total
     ? `${(value / 680) * 100}%`
     : `${(value / 260) * 100}%`;
+
+  barValue.classList.add(
+    total ? colorChart(value / 680) : colorChart(value / 260)
+  );
 
   const bar = document.createElement("div");
   bar.classList.add("stat-bar");
@@ -132,10 +221,12 @@ const pokemonStat = (stat, value, total) => {
 };
 
 // Función que retorna el body del pokemon
-const pokemonBody = () => {
+const pokemonBody = (sprites, stats) => {
   // image
   const image = document.createElement("img");
-  image.src = `./imgs/Dragonite.svg`;
+  // image.src = sprites.other;
+  image.src = sprites.home;
+  // image.src = sprites.official;
   image.alt = `dragonite`;
 
   const imageDiv = document.createElement("div");
@@ -143,100 +234,110 @@ const pokemonBody = () => {
   imageDiv.appendChild(image);
 
   // stats
-  const stats = document.createElement("div");
-  stats.classList.add("pokemon-stats");
-  stats.appendChild(pokemonStat("PS", 91));
-  stats.appendChild(pokemonStat("Ataque", 134));
-  stats.appendChild(pokemonStat("Defensa", 95));
-  stats.appendChild(pokemonStat("At. esp", 100));
-  stats.appendChild(pokemonStat("Def. esp", 100));
-  stats.appendChild(pokemonStat("Velocidad", 100));
-  stats.appendChild(pokemonStat("Total", 600, true));
+  const statsElement = document.createElement("div");
+  statsElement.classList.add("pokemon-stats");
+  statsElement.appendChild(pokemonStat("PS", stats.hp));
+  statsElement.appendChild(pokemonStat("Ataque", stats.attack));
+  statsElement.appendChild(pokemonStat("Defensa", stats.defense));
+  statsElement.appendChild(pokemonStat("At. esp", stats.speAtt));
+  statsElement.appendChild(pokemonStat("Def. esp", stats.speDef));
+  statsElement.appendChild(pokemonStat("Velocidad", stats.speed));
+  statsElement.appendChild(pokemonStat("Total", stats.total, true));
 
   const body = document.createElement("div");
   body.classList.add("pokemon-body");
   body.appendChild(imageDiv);
-  body.appendChild(stats);
+  body.appendChild(statsElement);
   return body;
 };
 
 // Función que retorna los movimientos del pokemon
-const pokemonMove = (name) => {
+const pokemonMove = (move) => {
   const moveName = document.createElement("span");
-  moveName.textContent = name;
+  moveName.textContent = move.name;
 
-  const type = document.createElement("img");
-  type.src = `/imgs/type/normal.svg`;
-  type.alt = "normal";
+  const typeImg = document.createElement("img");
+  typeImg.src = `/imgs/type/${move.type}.svg`;
+  typeImg.alt = move.type;
 
-  const category = document.createElement("img");
-  category.src = "./imgs/move/fisico.png";
-  category.alt = "fisico";
+  const catImg = document.createElement("img");
+  catImg.src = `./imgs/move/${move.damage_class}.png`;
 
-  const move = document.createElement("div");
-  move.classList.add("pokemon-move");
-  move.appendChild(moveName);
-  move.appendChild(type);
-  move.appendChild(category);
-  return move;
+  const moveElement = document.createElement("div");
+  moveElement.classList.add("pokemon-move");
+  moveElement.appendChild(moveName);
+  moveElement.appendChild(typeImg);
+  moveElement.appendChild(catImg);
+  return moveElement;
 };
 
 // Función que retorna el footer del pokemon
-const pokemonFooter = () => {
+const pokemonFooter = (ability, nature, moves) => {
   // ability
   const abilityLabel = document.createElement("span");
   abilityLabel.textContent = "Habilidad:";
 
   const abilityValue = document.createElement("span");
-  abilityValue.textContent = `Multiescamas`;
+  abilityValue.textContent = ability;
 
-  const ability = document.createElement("div");
-  ability.classList.add("pokemon-ability");
-  ability.appendChild(abilityLabel);
-  ability.appendChild(abilityValue);
+  const abilityDiv = document.createElement("div");
+  abilityDiv.classList.add("pokemon-ability");
+  abilityDiv.appendChild(abilityLabel);
+  abilityDiv.appendChild(abilityValue);
 
   // nature
   const natureLabel = document.createElement("span");
   natureLabel.textContent = "Naturaleza:";
 
   const natureValue = document.createElement("span");
-  natureValue.textContent = `Fuerte`;
+  natureValue.textContent = nature;
 
-  const nature = document.createElement("div");
-  nature.classList.add("pokemon-nature");
-  nature.appendChild(natureLabel);
-  nature.appendChild(natureValue);
+  const natureElement = document.createElement("div");
+  natureElement.classList.add("pokemon-nature");
+  natureElement.appendChild(natureLabel);
+  natureElement.appendChild(natureValue);
 
   // details
   const details = document.createElement("div");
   details.classList.add("pokemon-details");
-  details.appendChild(ability);
-  details.appendChild(nature);
+  details.appendChild(abilityDiv);
+  details.appendChild(natureElement);
+
+  // console.log(moves);
 
   // moves
-  const moves = document.createElement("div");
-  moves.classList.add("pokemon-moves");
-  moves.appendChild(pokemonMove("ultra puño"));
-  moves.appendChild(pokemonMove("danza dragon"));
-  moves.appendChild(pokemonMove("terremoto"));
-  moves.appendChild(pokemonMove("velocidad extrema"));
+  const movesElement = document.createElement("div");
+  movesElement.classList.add("pokemon-moves");
+  movesElement.appendChild(pokemonMove(moves[0]));
+  movesElement.appendChild(pokemonMove(moves[1]));
+  movesElement.appendChild(pokemonMove(moves[2]));
+  movesElement.appendChild(pokemonMove(moves[3]));
 
   // footer
   const footer = document.createElement("div");
   footer.classList.add("pokemon-footer");
   footer.appendChild(details);
-  footer.appendChild(moves);
+  footer.appendChild(movesElement);
   return footer;
 };
 
 // Función para mostrar la información del Pokémon
-const pokemonInfo = ({ id, name, types, sprites, stats }) => {
+const pokemonInfo = ({
+  id,
+  name,
+  types,
+  sprites,
+  stats,
+  ability,
+  nature,
+  moves,
+}) => {
   // pokemon-card
   const article = document.createElement("article");
   article.classList.add("pokemon-card");
   article.appendChild(pokemonHeader(id, name, types));
-  article.appendChild(pokemonBody());
-  article.appendChild(pokemonFooter());
+  article.appendChild(pokemonBody(sprites, stats));
+  article.appendChild(pokemonFooter(ability, nature, moves));
   return article;
 };
 
@@ -253,3 +354,6 @@ const getRandomTeam = async () => {
 };
 // Controllers
 buttonTeam.addEventListener("click", getRandomTeam);
+
+// deoxis
+// 386, 10001, 10002, 10003
